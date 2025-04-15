4bbe64ea88fa6849ed2e71d206c5bffa97611e61
Zachary Warfield <133118238+Warhead114@users.noreply.github.com>
2025-04-15 07:59:32 -0400

    Upload miscellaneous files

---
+++ replit.nix
@@ -0,0 +1,13 @@
+{pkgs}: {
+  deps = [
+    pkgs.pkg-config
+    pkgs.librsvg
+    pkgs.giflib
+    pkgs.libjpeg
+    pkgs.libpng
+    pkgs.pango
+    pkgs.cairo
+    pkgs.libuuid
+    pkgs.postgresql
+  ];
+}
